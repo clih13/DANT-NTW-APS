3233da1f9b77dc6dd505510422af1aa501fa03ee
Clih <aquino.christopher1396@gmail.com>
2019-02-25 15:53:09 +0100

    added function setData

--- dant/src/main/java/com/dant/app/Index.java
+++ dant/src/main/java/com/dant/app/Index.java
@@ -1,5 +1,8 @@
 package com.dant.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,6 +12,35 @@ public class Index {
 
 	public Index() {
 		this.lignes = new HashMap<String, ArrayList<String>>();
+	}
+	
+	public void SetData(int colonne) {
+		BufferedReader reader;
+		try {
+			File file = new File("tripdata_exemple.csv");
+			reader = new BufferedReader(new FileReader(file));
+			String line ;
+			String[] value;
+			
+			for(int i=0; i<2; i++) {
+				line = reader.readLine();
+			}
+			
+			while((line = reader.readLine()) != null) {
+				value = parse(line);
+				ArrayList<String> values = new ArrayList<String>();
+				for(int i = 0; i<value.length; i++) {
+					values.add(value[i]);
+				}
+				lignes.put(value[colonne-1], values);
+			}
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	public String[] parse(String line) {
+		return line.split(",");
 	}
 	
 }
